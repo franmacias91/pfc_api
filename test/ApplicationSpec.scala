88dbe8daad3b27661edec2dc90fc810d916b45ba
Rafa Paradela <rafa.p@47deg.com>
2015-11-26 13:17:49 +0100

    Cleaned tests

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -5,11 +5,6 @@ import org.junit.runner._
 import play.api.test._
 import play.api.test.Helpers._
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
 @RunWith(classOf[JUnitRunner])
 class ApplicationSpec extends Specification {
 
@@ -23,7 +18,6 @@ class ApplicationSpec extends Specification {
       val home = route(FakeRequest(GET, "/")).get
 
       status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
       contentAsString(home) must contain ("Your new application is ready.")
     }
   }
